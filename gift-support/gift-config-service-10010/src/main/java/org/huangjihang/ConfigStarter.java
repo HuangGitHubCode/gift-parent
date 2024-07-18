8c2e97e75d139bcf5031bdc37810536484ebeaaa
黄及航 <1123109673@qq.com>
2024-07-18 12:54:45 +0800

    初始化

--- gift-support/gift-config-service-10010/src/main/java/org/huangjihang/ConfigStarter.java
+++ gift-support/gift-config-service-10010/src/main/java/org/huangjihang/ConfigStarter.java
@@ -1,9 +1,13 @@
 package org.huangjihang;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigStarter.class,args);
+    }
 }
